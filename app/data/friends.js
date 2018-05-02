7be55f487293bec7b4c1fb1669d870b3adb2d903
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-01 21:08:07 -0400

    Comment out steps forward for javascript

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,10 @@
 //Contains all arrays of friends in JSON
 
-{
+//IMPORT post requests
+
+//EXPORT characters
+
+let phil = {
     "name":"Phil",
     "photo":"https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
     "scores":[
