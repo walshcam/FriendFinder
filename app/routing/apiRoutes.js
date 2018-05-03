0e968d4fef4721ed8fdaebc13da5e2e18e085995
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-02 21:52:21 -0400

    Create functionality for Submit button on survey.html. Untested

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,17 @@
 //Has a GET route to "/api/friends" that will display a JSON of all friends
+let characters = require('./../data/friends.js')
 
 module.exports = function(app, path) {
 
-    //Create a GET route to /survey
+    //Create a GET route to to create a json list of people.
     app.get("/api/friends", function(req, res) {
-        res.sendFile(path.join(__dirname,"./../data/friends.js"));
+        res.json(characters);
     });
 
-    //Have a default route that leads to home.html
-    app.post("/api/friends", function(req, res) {
-        res.sendFile(path.join(__dirname,"./../data/friends.js"));
-    });
+    // //Create a POST request to create new people
+    // app.post("/api/friends", function(req, res) {
+    //     res.sendFile(path.join(__dirname,"./../data/friends.js"));
+    // });
 
 }
 
